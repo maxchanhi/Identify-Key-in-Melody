ca7a99e93407bdc944da75f2aab00367b0359fc7
Chak Hang Chan <102423197+maxchanhi@users.noreply.github.com>
2024-04-25 06:16:02 +0100

    Update score_generation.py

--- score_generation.py
+++ score_generation.py
@@ -79,8 +79,8 @@ def lilypond_generation(melody, name, uppertime, lowertime):
                    check=True)
 
     # Generate MP3 file
-    subprocess.run(['fluidsynth', '-ni', '/Users/chakhangchan/Documents/VS_code/Music_theory_app/melody_key/GeneralUser/GeneralUserGSv1.471.sf2', 'score.midi', '-F', f'static/{name}.mp3', '-r', '44100'],
-               check=True)
+    # subprocess.run(['fluidsynth', '-ni', '/Users/chakhangchan/Documents/VS_code/Music_theory_app/melody_key/GeneralUser/GeneralUserGSv1.471.sf2', 'score.midi', '-F', f'static/{name}.mp3', '-r', '44100'],
+               # check=True)
 
     with Image.open('score.png') as img:
         width, height = img.size
@@ -91,64 +91,3 @@ def lilypond_generation(melody, name, uppertime, lowertime):
         os.makedirs('static', exist_ok=True)
         cropped_img.save(f'static/cropped_score_{name}.png')
     return f'static/cropped_score_{name}.png'
-
-def lilypond_homophonic(melody,harmony, name, uppertime, lowertime):
-    lilypond_score = f"""
-\\version "2.24.1"
-\\header {{
-    tagline = ""
-}}
-
-#(set-global-staff-size 26)
-
-\\score {{
-    \\new PianoStaff \\with {{
-        instrumentName = "Piano"
-    }} <<
-        \\new Staff = "upper" {{
-            \\fixed c'' {{
-                \\clef treble
-                \\time {uppertime}/{lowertime}
-                {melody}
-                \\bar "|"
-            }}
-        }}
-        \\new Staff = "lower" {{
-            \\fixed c {{
-                \\clef bass
-                \\time {uppertime}/{lowertime}
-                {harmony}
-                \\bar "|"
-            }}
-        }}
-    >>
-    \\layout {{ }}
-    \\midi {{ }}
-}}
-"""
-
-    with open('score.ly', 'w') as f:
-        f.write(lilypond_score)
-
-    # Generate PNG image and MIDI file
-    subprocess.run(['lilypond', '-dpreview', '-dbackend=eps', '--png', '-dresolution=300', '--output=score', 'score.ly'],
-                   check=True)
-
-    # Generate MP3 file
-    subprocess.run(['fluidsynth', '-ni', '/Users/chakhangchan/Documents/VS_code/Music_theory_app/melody_key/GeneralUser/GeneralUserGSv1.471.sf2', 'score.midi', '-F', f'static/{name}.mp3', '-r', '44100'],
-               check=True)
-
-    with Image.open('score.png') as img:
-        width, height = img.size
-        crop_height = height
-        crop_rectangle = (0, 75, width, crop_height / 10)
-        cropped_img = img.crop(crop_rectangle)
-
-        os.makedirs('static', exist_ok=True)
-        cropped_img.save(f'static/cropped_score_{name}.png')
-    return f'static/cropped_score_{name}.png'
-
-"""def score_generation(melody):
-  #melody = format_melody(melody)
-  lilypond_generation(melody,name="testing",uppertime=4,lowertime=4)
-"""
